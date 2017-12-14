e15f524d50a014cdb12414290b287609d1eb4255
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2017-12-14 16:48:05 +1100

    no message

--- WebAPITest1/Controllers/PartnersController.cs
+++ WebAPITest1/Controllers/PartnersController.cs
@@ -4,27 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPITest1.CsvHelper;
 using WebAPITest1.Models;
 
 namespace WebAPITest1.Controllers
 {
     public class PartnersController : ApiController
     {
-        List<Partner> Partners = new List<Partner>
-                {
-            new Partner { id = 1, name="ABC Partner", address="Sydney", phoneNumber = "1300 111 222"},
-            new Partner { id = 2, name="Awesome Partner", address="Sydney", phoneNumber = "1300 111 222"},
-            new Partner { id = 3, name="Excellent Partner", address="Sydney", phoneNumber = "1300 111 222"},
-            new Partner { id = 4, name="Gold Partner", address="Melbourne", phoneNumber = "1300 111 222"},
-                };
+        //List<Partner> Partners = new List<Partner>
+        //        {
+        //    new Partner { id = 1, name="ABC Partner", address="Sydney", phoneNumber = "1300 111 222"},
+        //    new Partner { id = 2, name="Awesome Partner", address="Sydney", phoneNumber = "1300 111 222"},
+        //    new Partner { id = 3, name="Excellent Partner", address="Sydney", phoneNumber = "1300 111 222"},
+        //    new Partner { id = 4, name="Gold Partner", address="Melbourne", phoneNumber = "1300 111 222"},
+
+        //};
+
+        private string GetFilePath()
+        {
+            return string.Format(@"{0}\{1}", System.Web.Hosting.HostingEnvironment.MapPath("/"), "partners.csv");
+        }
 
         public IEnumerable<Partner> GetAllPartners()
         {
-            return Partners;
+            return new CsvService().ReadPartners(GetFilePath());
         }
 
         public IHttpActionResult GetPartner(int id)
         {
+            var Partners = GetAllPartners();
+
             var Partner = Partners.FirstOrDefault((p) => p.id == id);
             if (Partner == null)
             {
@@ -36,7 +45,12 @@ namespace WebAPITest1.Controllers
         [HttpPost]
         public IHttpActionResult InsertPartner(Partner p)
         {
+            var Partners = GetAllPartners().ToList();
+
             Partners.Add(p);
+
+            new CsvService().WritePartners(Partners, GetFilePath());
+
             return Ok();
         }
 
---
+++ WebAPITest1/CsvHelper/CsvService.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using WebAPITest1.Models;
+
+namespace WebAPITest1.CsvHelper
+{
+    public class CsvService
+    {
+        public IEnumerable<Partner> ReadPartners(string filePath)
+        {
+            using (var stream = File.OpenText(filePath))
+            {
+
+                var csv = new CsvReader(stream);
+                return csv.GetRecords<Partner>().ToList();
+
+            }
+        }
+
+        public void WritePartners(IEnumerable<Partner> partners, string filePath)
+        {
+            using (var stream = File.CreateText(filePath))
+            {
+                var csv = new CsvWriter(stream);
+                csv.WriteRecords(partners);
+            }
+        }
+
+    }
+}
