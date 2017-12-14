eb5b50fcae735825c7335a9a3fd8e8b64a722372
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2017-12-14 15:07:18 +1100

    no message

---
+++ WebAPITest1/Controllers/PartnersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPITest1.Models;
+
+namespace WebAPITest1.Controllers
+{
+    public class PartnersController : ApiController
+    {
+        List<Partner> Partners = new List<Partner>
+                {
+            new Partner { id = 1, name="ABC Partner", address="Sydney", phoneNumber = "1300 111 222"},
+            new Partner { id = 2, name="Awesome Partner", address="Sydney", phoneNumber = "1300 111 222"},
+            new Partner { id = 3, name="Excellent Partner", address="Sydney", phoneNumber = "1300 111 222"},
+            new Partner { id = 4, name="Gold Partner", address="Melbourne", phoneNumber = "1300 111 222"},
+                };
+
+        public IEnumerable<Partner> GetAllPartners()
+        {
+            return Partners;
+        }
+
+        public IHttpActionResult GetPartner(int id)
+        {
+            var Partner = Partners.FirstOrDefault((p) => p.id == id);
+            if (Partner == null)
+            {
+                return NotFound();
+            }
+            return Ok(Partner);
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertPartner(Partner p)
+        {
+            Partners.Add(p);
+            return Ok();
+        }
+
+    }
+}
---
+++ WebAPITest1/Models/Partner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPITest1.Models
+{
+    public class Partner
+    {
+        public int  id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
